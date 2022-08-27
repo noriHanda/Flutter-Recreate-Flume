2a41fc0274f2758b84683910a34e8037b9d912ac
noriHanda <noriakihanda@gmail.com>
2022-08-27 11:04:22 +0900

    Successful skia build

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,13 +7,26 @@ plugins {
 
 group = "me.norihanda"
 version = "1.0-SNAPSHOT"
+val skArtifact = "skiko-awt-runtime-macos-x64"
+val skVersion = "0.7.30"
+val lwjglVersion = "3.3.1"
+val lwjglNatives = "natives-macos"
 
 repositories {
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    api("org.jetbrains.skiko:${skArtifact}:${skVersion}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.lwjgl:lwjgl:$lwjglVersion")
+    implementation("org.lwjgl:lwjgl-glfw:$lwjglVersion")
+    implementation("org.lwjgl:lwjgl-opengl:$lwjglVersion")
+    runtimeOnly("org.lwjgl:lwjgl:$lwjglVersion:$lwjglNatives")
+    runtimeOnly("org.lwjgl:lwjgl-glfw:$lwjglVersion:$lwjglNatives")
+    runtimeOnly("org.lwjgl:lwjgl-opengl:$lwjglVersion:$lwjglNatives")
 }
 
 tasks.test {
