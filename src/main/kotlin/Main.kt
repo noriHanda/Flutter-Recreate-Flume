4add25a80bc92076456b450bed91791e46ca0f6c
noriHanda <noriakihanda@gmail.com>
2022-08-27 20:40:17 +0900

    Create task runners

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,51 +1,41 @@
-import org.jetbrains.skia.*
-import org.lwjgl.glfw.GLFW.*
-import org.lwjgl.opengl.GL
-import org.lwjgl.opengl.GL11
+import engine.*
+import org.lwjgl.glfw.GLFW.GLFW_KEY_M
+import org.lwjgl.glfw.GLFW.GLFW_PRESS
 
-fun main(args: Array<String>) {
+fun main() {
     val width = 640
     val height = 480
 
-    glfwInit()
-    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE)
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE)
-    val windowHandle = glfwCreateWindow(width, height, "Flume", 0, 0)
-    glfwMakeContextCurrent(windowHandle)
-    glfwSwapInterval(1)
-    glfwShowWindow(windowHandle)
+    val taskRunners = TaskRunners(rasterTaskRunner = TaskRunner(), uiTaskRunner = TaskRunner())
 
+    val glView = GLView(width, height)
 
-    GL.createCapabilities()
+    val shell = Shell(taskRunners, glView, null)
 
-    val context = DirectContext.makeGL()
-
-    val fbId = GL11.glGetInteger(0x8CA6)
-    val renderTarget =
-        BackendRenderTarget.makeGL(width, height, 0, 8, fbId, FramebufferFormat.GR_GL_RGBA8)
-
-    val surface = Surface.makeFromBackendRenderTarget(context,
-        renderTarget,
-        SurfaceOrigin.BOTTOM_LEFT,
-        SurfaceColorFormat.RGBA_8888,
-        ColorSpace.sRGB)!!
-
-    val canvas = surface.canvas
-
-    while (!glfwWindowShouldClose(windowHandle)) {
-
-        val paintGreen = Paint().apply { color = 0xFF8BC34A.toInt() }
-        canvas.drawCircle(100f, 100f, 40f, paintGreen)
-
-        val paintYellow = Paint().apply { color = 0xFFFFEB3B.toInt() }
-        canvas.drawCircle(200f, 100f, 40f, paintYellow)
-
-        val paintRed = Paint().apply { color = 0xFFF44336.toInt() }
-        canvas.drawCircle(300f, 100f, 40f, paintRed)
-
-        context.flush()
-        glfwSwapBuffers(windowHandle)
-        glfwPollEvents()
+    taskRunners.rasterTaskRunner.postTask {
+        println("in rasterThread")
+        val context = shell.glView.createContext()
+        val rasterizer = Rasterizer(width, height, context)
+        shell.rasterizer = rasterizer
     }
 
+    var keyPressed = false
+
+    glView.setKeyCallback { _, key, _, action, _ ->
+        if (key == GLFW_KEY_M && action == GLFW_PRESS) {
+            keyPressed = true
+        }
+    }
+
+    while (!shell.glView.windowShouldClose()) {
+        if (keyPressed) {
+            keyPressed = false
+            shell.taskRunners.rasterTaskRunner.postTask {
+                shell.rasterizer!!.drawToSurface()
+                shell.glView.swapBuffers()
+            }
+        }
+        shell.glView.pollEvents()
+    }
+    shell.taskRunners.terminateAll()
 }
