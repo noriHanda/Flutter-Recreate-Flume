abe246db485ecd5b7a09144e0406dae5200b805c
noriakihanda <noriaki.handa+github@h-medtech.com>
2022-09-03 08:53:28 +0900

    Implement layout tree

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,14 @@
+import common.ContainerLayer
+import common.LayerTree
+import common.OpacityLayer
+import common.PictureLayer
 import engine.*
+import org.jetbrains.skia.Paint
+import org.jetbrains.skia.PictureRecorder
+import org.jetbrains.skia.Rect
 import org.lwjgl.glfw.GLFW.GLFW_KEY_M
 import org.lwjgl.glfw.GLFW.GLFW_PRESS
+import kotlin.random.Random
 
 fun main() {
     val width = 640
@@ -31,11 +39,46 @@ fun main() {
         if (keyPressed) {
             keyPressed = false
             shell.taskRunners.rasterTaskRunner.postTask {
-                shell.rasterizer!!.drawToSurface()
+                shell.rasterizer!!.drawToSurface(
+                    createRandomTree(width.toFloat(), height.toFloat())
+                )
                 shell.glView.swapBuffers()
             }
         }
         shell.glView.pollEvents()
     }
     shell.taskRunners.terminateAll()
+}
+
+fun createRandomTree(width: Float, height: Float): LayerTree {
+    val root = ContainerLayer()
+    val rect = Rect.makeXYWH(0f, 0f, width, height)
+    val leaf = PictureLayer()
+    val recorder = PictureRecorder()
+    val canvas = recorder.beginRecording(rect)
+
+    val paint = Paint().apply { color = 0xFFFF0000.toInt() }
+
+    val randomX = Random.nextFloat() * width
+    val randomY = Random.nextFloat() * height
+
+    canvas.drawCircle(randomX, randomY, 40f, paint)
+
+    leaf.picture = recorder.finishRecordingAsPicture()
+    root.children.add(leaf)
+
+    val opacity = OpacityLayer(alpha = 150)
+
+    val opacityPicture = PictureLayer()
+    val opacityRecorder = PictureRecorder()
+    val opacityCanvas = opacityRecorder.beginRecording(rect)
+    opacityCanvas.drawCircle(0f, 0f, 60f, paint)
+    opacityPicture.picture = opacityRecorder.finishRecordingAsPicture()
+    opacity.children.add(opacityPicture)
+
+    root.children.add(opacity)
+
+    return LayerTree().apply {
+        rootLayer = root
+    }
 }
---
+++ src/main/kotlin/common/LayerTree.kt
@@ -0,0 +1,177 @@
+package common
+
+import org.jetbrains.skia.*
+
+class LayerTree {
+    var rootLayer: Layer? = null
+
+    /**
+     * LayerツリーのそれぞれのLayerのpaintBoundsを決定する
+     */
+    fun preroll() {
+        assert(rootLayer != null)
+
+        rootLayer!!.preroll()
+    }
+
+    /**
+     * PaintContextを用いてLayerツリーを描画する
+     */
+    fun paint(context: PaintContext) {
+        rootLayer?.paint(context)
+    }
+}
+
+abstract class Layer {
+    var paintBounds: Rect = Rect.makeWH(0f, 0f)
+
+    abstract fun paint(context: PaintContext)
+    abstract fun preroll()
+}
+
+open class ContainerLayer : Layer() {
+    val children: MutableList<Layer> = mutableListOf()
+
+    override fun preroll() {
+        paintBounds = prerollChildren()
+    }
+
+    /**
+     * 子の矩形を計算しその和を返す
+     */
+    protected fun prerollChildren(): Rect {
+        var bounds = kEmptyRect
+        for (child in children) {
+            child.preroll()
+            bounds = bounds.join(child.paintBounds)
+        }
+        return bounds
+    }
+
+    override fun paint(context: PaintContext) {
+        for (child in children) {
+            child.paint(context)
+        }
+    }
+}
+
+class PictureLayer : Layer() {
+    var picture: Picture? = null
+
+    override fun preroll() {
+        paintBounds = picture!!.cullRect
+    }
+
+    override fun paint(context: PaintContext) {
+        picture?.playback(context.canvas)
+    }
+}
+
+class TransformLayer(
+    var transform: Matrix33 = Matrix33.IDENTITY,
+) : ContainerLayer() {
+    companion object {
+        fun withOffset(
+            transform: Matrix33 = Matrix33.IDENTITY,
+            offset: Offset = Offset.zero,
+        ): TransformLayer {
+            val move = Matrix33.makeTranslate(offset.dx.toFloat(), offset.dy.toFloat())
+            return TransformLayer(transform.makeConcat(move))
+        }
+    }
+
+    override fun preroll() {
+        val childPaintBounds = prerollChildren()
+        paintBounds = transform.mapRect(childPaintBounds)
+    }
+
+    override fun paint(context: PaintContext) {
+        context.canvas.save()
+        context.canvas.concat(transform)
+
+        super.paint(context)
+
+        context.canvas.restore()
+    }
+}
+
+class OpacityLayer(
+    var alpha: Int? = null
+) : ContainerLayer() {
+    override fun paint(context: PaintContext) {
+        val paint = Paint()
+        if (alpha != null) {
+            paint.alpha = alpha!!
+        }
+        context.canvas.saveLayer(paintBounds.roundOut(), paint)
+        super.paint(context)
+        context.canvas.restore()
+    }
+}
+
+class ClipPathLayer(
+    var clipPath: Path, var clipBehavior: Clip = Clip.AntiAlias,
+) : ContainerLayer() {
+    override fun preroll() {
+        val clipPathBounds = clipPath.bounds
+        val childPaintBounds = prerollChildren()
+        if (childPaintBounds.intersect(clipPathBounds) != null) {
+            paintBounds = childPaintBounds
+        }
+    }
+
+    override fun paint(context: PaintContext) {
+        context.canvas.save()
+        context.canvas.clipPath(clipPath, clipBehavior != Clip.HardEdge)
+
+        super.paint(context)
+        context.canvas.restore()
+    }
+}
+
+class ClipRectLayer(
+    var clipRect: Rect, var clipBehavior: Clip = Clip.AntiAlias,
+) : ContainerLayer() {
+    override fun preroll() {
+        val childPaintBounds = prerollChildren()
+        if (childPaintBounds.intersect(clipRect) != null) {
+            paintBounds = childPaintBounds
+        }
+    }
+
+    override fun paint(context: PaintContext) {
+        context.canvas.save()
+        context.canvas.clipRect(clipRect, clipBehavior != Clip.HardEdge)
+
+        super.paint(context)
+        context.canvas.restore()
+    }
+}
+
+class ClipRRectLayer(
+    var clipRRect: RRect, var clipBehavior: Clip = Clip.AntiAlias,
+) : ContainerLayer() {
+    override fun preroll() {
+        val childPaintBounds = prerollChildren()
+        if (childPaintBounds.intersect(clipRRect) != null) {
+            paintBounds = childPaintBounds
+        }
+    }
+
+    override fun paint(context: PaintContext) {
+        context.canvas.save()
+        context.canvas.clipRRect(clipRRect, clipBehavior != Clip.HardEdge)
+
+        super.paint(context)
+        context.canvas.restore()
+    }
+}
+
+enum class Clip {
+    None, HardEdge, AntiAlias
+}
+
+data class PaintContext(
+    val canvas: Canvas,
+    val context: DirectContext,
+)
